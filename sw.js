6463c7bea87ff81a873315ca0eb35c3d564fe205
pksking <kprithiv1010@gmail.com>
2026-01-08 18:16:34 +0530

    ADD PWA support

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response("Offline");
+    })
+  );
+});
